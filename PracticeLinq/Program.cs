1c81eec0cad8564870ff5c26cbb74f8ca083a8c7
Kaylie Phillips <kaylie.travel@gmail.com>
2022-09-29 17:43:17 -0500

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+//changed topic from video games to board game (I know more of them. lol)
+
+List<string> boardGames = new List<string>() { "Monopoly", "Cashflow", "Clue", "Scrabble","Settlers of Catan",
+    "Ticket to Ride", "Risk" };
+
+//order the list of games by length of game name.  
+
+var nameLength = boardGames.OrderBy(x => x.Length);
+
+foreach (var game in nameLength)
+{
+    Console.WriteLine(game);
+
+}
